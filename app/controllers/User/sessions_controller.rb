1332e100d140250bbf5d06475533574489155abd
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2023-10-31 00:13:28 +0530

    Fix/designing (#5)
    * add tailwind css gem and configuration
    
    * add home and dashboard image in assets
    
    * create welcome controller
    
    * Fix UI
    
    * add routes and fix login sign up UI
    
    * fix bug

--- app/controllers/User/sessions_controller.rb
+++ app/controllers/User/sessions_controller.rb
@@ -3,10 +3,12 @@
 class User::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+
   # GET /resource/sign_in
   # def new
   #   super
   # end
+ 
 
   # POST /resource/sign_in
   # def create
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ApplicationRecord
-    has_one_attached :image
+    has_one_attached :image, :dependent=> :destroy
+
+    def image_url
+        if image.attached?
+          Rails.application.routes.url_helpers.rails_blob_path image.blob, host: :localhost
+        else
+          ''
+        end
+      end
 end
