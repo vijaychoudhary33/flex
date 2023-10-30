c11d856435507c2a16acc1bce02da73ca5300bd5
Vijay Choudhary <vijaychoudharyin@gmail.com>
2023-10-31 00:06:56 +0530

    add routes and fix login sign up UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  # get 'welcome/index', to: 'welcome#index', as: 'welcome'
   devise_for :user, controllers: {
     sessions: 'user/sessions' }
   resources :movies
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "movies#index"
+  root "welcome#index"
   namespace :admin do
     resources :movies
     resources :user
